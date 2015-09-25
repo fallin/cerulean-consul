7f13eba21ec23858b7ae35b225ae0d21693885dd
Brant Fallin <brant.fallin@gmail.com>
2015-09-24 21:18:59 -0600

    Improve create-package.cmd
    Update the nuget package script to automatically build the release configuration and then create the nuget package.

--- deploy/create-package.cmd
+++ deploy/create-package.cmd
@@ -4,6 +4,25 @@
 :: where the CMD file was run from, so change directory...
 cd /d %~dp0
 
-echo info: Create NuGet package
+echo info: Build cerulean-consul
+call "%VS120COMNTOOLS%\vsvars32.bat"
+msbuild ..\cerulean-consul.sln /t:Rebuild /p:Configuration=Release
+if ERRORLEVEL 1 (
+    echo error: Failed to build solution
+    goto ERROR
+)
 
+echo info: Create NuGet package
 nuget.exe pack .\Cerulean.Consul.nuspec
+if ERRORLEVEL 1 (
+    echo error: Failed to create nuget package
+    goto ERROR
+)
+
+goto END
+
+:ERROR
+pause
+
+:END
+echo info: Complete
